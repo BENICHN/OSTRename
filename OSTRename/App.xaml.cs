cc25614d0c762807fc5cdfbbb698412709c86ae7
BenNatNB <nathan.benichou31280@gmail.com>
2018-04-14 18:04:45 +0200

    Ajoutez des fichiers projet.

---
+++ OSTRename/App.xaml.cs
@@ -0,0 +1,82 @@
+﻿using AsyncIO.FileSystem;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+using BenLib;
+using Z.Linq;
+using System.Threading;
+
+namespace OSTRename
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public new static MainWindow MainWindow { get; private set; }
+
+        public static string OSTName { get; private set; }
+        public static string[] Pattern { get; private set; }
+        public static bool Number { get; set; }
+        public static string[][] Translations { get; private set; }
+
+        private void ShowError(bool isPatternError)
+        {
+            MessageBox.Show(isPatternError ? "L'en-tête du fichier de traductions n'est pas valide" : "Le corps du fichier de traductions n'est pas valide", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            Shutdown(3);
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            string[] file = null;
+
+            if (e.Args.Length > 0 && File.Exists(e.Args[0])) file = await AsyncFile.ReadAllLinesAsync(e.Args[0]);
+            else
+            {
+                var ofd = new OpenFileDialog { Title = "Sélectionnez un fichier de traductions" };
+
+                if (ofd.ShowDialog() == true && File.Exists(ofd.FileName)) file = await AsyncFile.ReadAllLinesAsync(ofd.FileName);
+                else
+                {
+                    Shutdown(2);
+                    return;
+                }
+            }
+
+            if (file.Length < 4) ShowError(true);
+            if (await file[0].AllAsync(c => c.Equals('=')) && await file[3].AllAsync(c => c.Equals('=')))
+            {
+                OSTName = file[1];
+                Number = file[2].StartsWith("⇒ ");
+                Pattern = file[2].TrimStart("⇒ ", 1).Split(new[] { " ⇔ " }, StringSplitOptions.None);
+                if (Pattern.Length <= 1 || await Pattern.AnyAsync(line => line.IsNullOrWhiteSpace()) || await (await Pattern.GroupByAsync(line => line)).AnyAsync(async group => await group.CountAsync() > 1)) ShowError(true);
+            }
+            else ShowError(true);
+
+            if (file.Length < 6) ShowError(false);
+
+            Translations = (await (await file.SubArray(5, file.Length - 5).SelectAsync(line =>
+            {
+                if (!Number ^ line.Contains(" ⇒ ") && line.Split(new[] { " ⇔ " }, StringSplitOptions.None).Length == Pattern.Length)
+                {
+                    if (Number)
+                    {
+                        var index = line.IndexOf(" ⇒ ");
+                        var number = line.Substring(0, index);
+                        var litt = line.Substring(index + 3);
+                        return new[] { number }.Concat(litt.Split(new[] { " ⇔ " }, StringSplitOptions.None)).ToArray();
+                    }
+                    else return line.Split(new[] { " ⇔ " }, StringSplitOptions.None);
+                }
+                else return null;
+            })).WhereAsync(line => line != null)).ToArray();
+
+            MainWindow = new MainWindow() { Title = $"{OSTName} - OSTRename" };
+            MainWindow.Closed += (sender, args) => Shutdown();
+            MainWindow.Show();
+        }
+    }
+}
---
+++ OSTRename/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using Z.Linq;
+using AsyncIO.FileSystem;
+using AsyncIO.FileSystem.Extensions;
+using BenLib;
+
+namespace OSTRename
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> Files { get; set; } = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileList.MaxHeight = SystemParameters.PrimaryScreenHeight / 2;
+            DataContext = this;
+        }
+
+        public async Task AddFiles(IEnumerable<string> collection = default)
+        {
+            OpenFileDialog ofd = null;
+            if (collection != null || (ofd = new OpenFileDialog() { Multiselect = true }).ShowDialog() == true)
+            {
+                Files = new ObservableCollection<string>(await Files.UnionAsync(collection ?? ofd.FileNames));
+                if (chbx_DisplayFilesInList.IsChecked == true) fileList.GetBindingExpression(ItemsControl.ItemsSourceProperty).UpdateTarget();
+            }
+        }
+
+        private async void btn_Add_Click(object sender, RoutedEventArgs e) => await AddFiles();
+
+        private async void btn_Rename_Click(object sender, RoutedEventArgs e)
+        {
+            if (Files.Count == 0) return;
+
+            var from = App.Pattern.IndexOf(cbx_From.SelectedItem);
+            var to = App.Pattern.IndexOf(cbx_To.SelectedItem);
+
+            if (App.Number)
+            {
+                from++;
+                to++;
+            }
+
+            int count = 0;
+            bar_Main.Maximum = Files.Count;
+
+            foreach (string file in Files.ToList())
+            {
+                var name = Path.GetFileNameWithoutExtension(file);
+                var translation = await App.Translations.FirstOrDefaultAsync(tr =>
+                {
+                    if (tr[from] == name) return true;
+                    if (App.Number)
+                    {
+                        var numLit = name.Split(new[] { " - " }, 2, StringSplitOptions.None);
+                        if (numLit.Length < 2) return false;
+                        return numLit[0] == tr[0] && numLit[1] == tr[from];
+                    }
+                    else return false;
+                });
+
+                bar_Main.Value++;
+                if (translation.IsNullOrEmpty()) continue;
+
+                var number = chbx_Num.IsChecked == true ? $"{translation[0]} - " : String.Empty;
+                var newName = Path.Combine(Path.GetDirectoryName(file), number + translation[to] + Path.GetExtension(file));
+                Files[Files.IndexOf(file)] = newName;
+
+                await AsyncFile.MoveAsync(file, newName);
+                count++;
+            }
+
+            MessageBox.Show($"{count} Fichiers renommés", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            bar_Main.Value = 0;
+        }
+
+        protected override async void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            switch(e.Key)
+            {
+                case Key.Insert:
+                    await AddFiles();
+                    break;
+                case Key.Delete:
+                    foreach (string file in await fileList.SelectedItems.OfTypeAsync<string>().ToList()) Files.Remove(file);
+                    break;
+            }
+        }
+
+        private async void Window_Drop(object sender, DragEventArgs e)
+        {
+            var files = ((string[])e.Data.GetData(DataFormats.FileDrop, false)).ToList();
+            foreach (string folder in files.Where(folder => Directory.Exists(folder)).ToList())
+            {
+                files.Remove(folder);
+                if (Keyboard.Modifiers == ModifierKeys.Shift) files.AddRange(Directory.GetFiles(folder, "*", SearchOption.AllDirectories));
+                else files.AddRange(Directory.GetFiles(folder));
+            }
+            await AddFiles(files);
+        }
+    }
+}
